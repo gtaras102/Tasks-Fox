4142313d630600e89333c2c50400304e86973c84
Taras <gtaras102@gmail.com>
2024-03-14 18:50:11 +0200

    new tasks

--- Task_4_Tsezar.cs
+++ Task_4_Tsezar.cs
@@ -3,3 +3,52 @@
 Зашифравати та розшифрувати текстове повідомлення
 */
 
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "";
+        int key;
+        //string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        Console.WriteLine("Введіть текст:");
+        text = Console.ReadLine();
+
+        Console.WriteLine("\nВведіть ключ для шифрування (від 1 до 5):)");
+        key = Convert.ToInt32(Console.ReadLine());
+            while (key < 0 || key > 5)
+            {
+                Console.WriteLine("Неприпустимий діапазон ключа. Введіть значення від 1 до 5.");
+                key = Convert.ToInt32(Console.ReadLine());   
+            }
+
+        string encryptedText = CaesarCipher(text, key);
+        Console.WriteLine("Encrypted Text: " + encryptedText);
+
+        string decryptedText = CaesarCipher(encryptedText, -key);
+        Console.WriteLine("Decrypted Text: " + decryptedText);
+    }
+    static string CaesarCipher(string message, int shift)
+    {
+        char[] buffer = message.ToCharArray();
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+
+            if (char.IsLetter(letter))
+            {
+                char letterOffset = char.IsUpper(letter) ? 'A' : 'a';
+                letter = (char)((((letter + shift) - letterOffset + 26) % 26) + letterOffset);
+            }
+
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+}
+
+
+
+
